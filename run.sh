0ce85c31df4b02fa536e248c7c732149a31f6e41
Jeremy Tan <jtanx@outlook.com>
2019-04-19 06:50:20 +0800

    sigh

--- run.sh
+++ run.sh
@@ -5,13 +5,13 @@ set -ex
 sudo apt-get -y install autoconf automake bison build-essential \
 	diffutils flex gawk git gperf help2man make patch libncurses5-dev \
 	texinfo wget xz-utils python python3 unzip libtool libtool-bin \
-	axel
+	aria2
 
-axel -n 6 https://sourceforge.net/projects/raspberry-pi-cross-compilers/files/Raspberry%20Pi%20GCC%20Cross-Compilers/GCC%206.3.0/Raspberry%20Pi%202%2C%203/cross-gcc-6.3.0-pi_2-3.tar.gz
+aria2c -x 8 https://sourceforge.net/projects/raspberry-pi-cross-compilers/files/Raspberry%20Pi%20GCC%20Cross-Compilers/GCC%206.3.0/Raspberry%20Pi%202%2C%203/cross-gcc-6.3.0-pi_2-3.tar.gz
 tar axf cross-gcc-6.3.0-pi_2-3.tar.gz
 export PATH=$PATH:`pwd`/cross-pi-gcc-6.3.0-1/bin/
 
-axel -n 5 http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-1.24.0.tar.bz2 && \
+aria2c -x 8 http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-1.24.0.tar.bz2 && \
 	tar axf crosstool-ng-1.24.0.tar.bz2 && \
 	pushd crosstool-ng-1.24.0 && \
 	./configure && \
